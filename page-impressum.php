1e8b2a87cedc00947fe3267219b596ffeb1173ce
Viktor <v.lavnejev@gmail.com>
2023-06-28 21:53:55 +0300

    Add Impressum and Datenschutz

---
+++ page-impressum.php
@@ -0,0 +1,79 @@
+<?php /* Template Name: Page Impressum */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+    <div class="container">
+        <h2><?php echo get_the_title(); ?></h2>
+
+        <section class="grayplate-text">
+            <div class="grayplate-text__wrap two-columns">
+                <div class="grayplate-text__column">
+                    <div class="subtitle">Herausgeber</div>
+                    <h6>KEMROC Spezialmaschinen GmbH</h6>
+
+                    <div class="subtitle">Anschrift</div>
+                    <h6>KEMROC Spezialmaschinen GmbHJeremiasstraße 436433 LeimbachDeutschland</h6>
+
+                    <div class="subtitle">USt-ID</div>
+                    <h6>DE313714551</h6>
+                </div>
+
+                <div class="grayplate-text__column">
+                    <div class="subtitle">Kontakt</div>
+                    <a href="tel:+49 3695 850 2550">Tel. +49 3695 850 2550</a>
+                    <a href="tel:+49 3695 850 2579">Fax +49 3695 850 2579</a>
+                    <a href="mailto:info@kemroc.de">E-mail: info@kemroc.de</a>
+
+                    <div class="subtitle">Eintrag im Handelsregister</div>
+                    <h6>Registergericht: Amtsgericht JenaRegisternummer: HRB 513785</h6>
+
+                    <div class="subtitle">Geschäftsführer</div>
+                    <h6 style="margin-bottom: 0;">Dipl.-Ing. Klaus Ertmer</h6>
+                </div>
+            </div>
+        </section>
+
+        <section class="columns-text">
+            <h3>Haftung für Inhalte</h3>
+            <div class="columns-text__wrap">
+                <p>Diese Website wurde mit größtmöglicher Sorgfalt zusammengestellt. Trotzdem kann keine Gewähr für die
+                    Fehlerfreiheit und Genauigkeit der enthaltenen Informationen übernommen werden. Jegliche Haftung für
+                    Schäden, die direkt oder indirekt aus der Benutzung dieser Website entstehen, wird ausgeschlossen.
+                </p>
+                <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen
+                    Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+                    Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
+                    Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+            </div>
+        </section>
+
+        <section class="columns-text">
+            <h3>Urheberrecht</h3>
+            <div class="columns-text__wrap">
+                <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem
+                    deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung
+                    außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors
+                    bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen
+                    Gebrauch gestattet.</p>
+                <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte
+                    Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie
+                    trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden
+                    Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+                </p>
+            </div>
+        </section>
+
+        <section class="columns-text">
+            <h6>Bildnachweis</h6>
+            <div class="columns-text__wrap">
+                <p>© MyFortis/iStock/Thinkstock</p>
+            </div>
+        </section>
+    </div>
+</main><!-- #main -->
+
+<?php
+
+get_footer();
