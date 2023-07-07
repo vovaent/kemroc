0a96337008b6874d962027eb16a9fd57c8ef553e
vovaent <vovaent@gmail.com>
2023-07-07 16:36:28 +0300

    Temp Google map added

--- template-parts/blocks/contacts/contacts-info/contacts-info.php
+++ template-parts/blocks/contacts/contacts-info/contacts-info.php
@@ -31,12 +31,13 @@ if ( ! $is_preview ) :
 	}
 
 	// Load values and assing defaults.
-	$kemroc_ci_personal_logo_use = get_field( 'personal_logo_use' );
-	$kemroc_ci_personal_logo     = get_field( 'personal_logo' );
-	$kemroc_ci_logo              = get_field( 'header_logo', 'option' );
-	$kemroc_ci_personal_logo     = $kemroc_ci_personal_logo_use ? $kemroc_ci_personal_logo : $kemroc_ci_logo;
-	$kemroc_ci_address           = get_field( 'address' );
-	$kemroc_ci_contacts          = get_field( 'contacts' );
+	$kemroc_ci_personal_logo_use    = get_field( 'personal_logo_use' );
+	$kemroc_ci_personal_logo        = get_field( 'personal_logo' );
+	$kemroc_ci_logo                 = get_field( 'header_logo', 'option' );
+	$kemroc_ci_personal_logo        = $kemroc_ci_personal_logo_use ? $kemroc_ci_personal_logo : $kemroc_ci_logo;
+	$kemroc_ci_address              = get_field( 'address' );
+	$kemroc_ci_contacts             = get_field( 'contacts' );
+	$kemroc_ci_google_map_shortcode = get_field( 'google_map_shortcode' );
 	?>
 
 	<section id="<?php echo esc_attr( $kemroc_ci_id ); ?>" class="<?php echo esc_attr( $kemroc_ci_class_name ); ?>">
@@ -75,8 +76,14 @@ if ( ! $is_preview ) :
 			</div>
 			<!-- /.contacts-info__text -->
 			<div class="contacts-info__map">
-				<img src="<?php echo get_template_directory_uri() . '/images/map.png'; ?>" alt="" class="contacts-info__map-image">
-				<img src="<?php echo get_template_directory_uri() . '/images/marker.svg'; ?>" alt="" class="contacts-info__map-marker">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.7229441983714!2d10.161076399999999!3d50.81777169999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a366d4879c54a7%3A0xdeccbd0ec6d842c2!2zQWhvcm5zdHJhw59lIDYsIDM2NDY5IEJhZCBTYWx6dW5nZW4sINCT0LXRgNC80LDQvdC40Y8!5e0!3m2!1sru!2sua!4v1688735318007!5m2!1sru!2sua" 
+					width="570" 
+					height="475" 
+					style="border:0;" 
+					allowfullscreen="" 
+					loading="lazy" 
+					referrerpolicy="no-referrer-when-downgrade"
+				></iframe>
 			</div>
 			<!-- /.contacts-info__map -->
 		</div>
