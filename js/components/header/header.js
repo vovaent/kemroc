0915ca1515233f46176f83fac463560f2b8b80ea
vovaent <vovaent@gmail.com>
2023-07-10 11:39:01 +0300

    Fix header with scroll

--- js/components/header/header.js
+++ js/components/header/header.js
@@ -11,14 +11,18 @@ const header = ( $ ) => {
 
 	const windowScrollHandler = function () {
 		const st = $document.scrollTop();
-		if ( st > lastScrollTop ) {
-			// downscroll code
-			if ( ! $header.hasClass( 'header--translate-to-up' ) ) {
-				$header.addClass( 'header--translate-to-up' );
-				$document.trigger( 'headerIsHidden' );
+		const isScrollDown = st > lastScrollTop;
+
+		if ( isScrollDown ) {
+			const headerIsScrolled = st > $header.height();
+
+			if ( headerIsScrolled ) {
+				if ( ! $header.hasClass( 'header--translate-to-up' ) ) {
+					$header.addClass( 'header--translate-to-up' );
+					$document.trigger( 'headerIsHidden' );
+				}
 			}
 		} else {
-			// upscroll code
 			$header.removeClass( 'header--translate-to-up' );
 			$document.trigger( 'headerIsVisible' );
 		}
