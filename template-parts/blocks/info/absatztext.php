60456af3a6035cc0960821010f7d00a1724888b3
vovaent <vovaent@gmail.com>
2023-07-12 18:25:49 +0300

    Fix info pages

--- template-parts/blocks/info/absatztext.php
+++ template-parts/blocks/info/absatztext.php
@@ -3,56 +3,57 @@
 /**
  * Absatztext Block Template.
  */
-if (isset($block['data']['gutenberg_preview_image']) && $is_preview) {
-  echo '<img src="' . esc_url($block['data']['gutenberg_preview_image']) . '" style="max-width:100%; height:auto;">';
+if ( isset( $block['data']['gutenberg_preview_image'] ) && $is_preview ) {
+	echo '<img src="' . esc_url( $block['data']['gutenberg_preview_image'] ) . '" style="max-width:100%; height:auto;">';
 }
 
-if (!$is_preview) :
-  $kemroc_absatztext_id = 'absatztext-' . $block['id'];
-  if (!empty($block['anchor'])) {
-    $kemroc_faq_id = $block['anchor'];
-  }
+if ( ! $is_preview ) :
+	$kemroc_absatztext_id = 'absatztext-' . $block['id'];
+	if ( ! empty( $block['anchor'] ) ) {
+		$kemroc_faq_id = $block['anchor'];
+	}
 
-  $kemroc_absatztext_class_name = 'absatztext';
-  if (!empty($block['className'])) {
-    $kemroc_absatztext_class_name .= ' ' . $block['className'];
-  }
-  if (!empty($block['align'])) {
-    $kemroc_absatztext_class_name .= ' align' . $block['align'];
-  }
+	$kemroc_absatztext_class_name = 'absatztext';
+	if ( ! empty( $block['className'] ) ) {
+		$kemroc_absatztext_class_name .= ' ' . $block['className'];
+	}
+	if ( ! empty( $block['align'] ) ) {
+		$kemroc_absatztext_class_name .= ' align' . $block['align'];
+	}
 
-  $seitenangabe_anzeigen = get_field('seitenangabe_anzeigen');
-  $title = get_field('title');
-  $rows = get_field('absatztext_item');
-?>
+	$seitenangabe_anzeigen = get_field( 'seitenangabe_anzeigen' );
+	$title                 = get_field( 'title' );
+	$rows                  = get_field( 'absatztext_item' );
+	?>
 
-  <section id="<?php echo esc_attr($kemroc_absatztext_id); ?>" class="<?php echo esc_attr($kemroc_absatztext_class_name); ?>">
+  <section id="<?php echo esc_attr( $kemroc_absatztext_id ); ?>" class="<?php echo esc_attr( $kemroc_absatztext_class_name ); ?>">
   <div class="container">
-  <?php if ($seitenangabe_anzeigen == 'Ja') : ?>
-    <h1><?php echo get_the_title(); ?></h1>
-    <?php endif; ?>
+	<?php if ( $seitenangabe_anzeigen == 'Ja' ) : ?>
+	<h1><?php echo get_the_title(); ?></h1>
+	<?php endif; ?>
   
-    
-      <div class="grayplate-text">
-        <?php if ($rows) {
-        ?>
-          <div class="grayplate-text__wrap">
-          <h2><?php echo $title; ?></h2>
-            <?php foreach ($rows as $row) { ?>
-              <?php if ($row['untertitel_1']) : ?>
-                <h3><?php echo $row['untertitel_1']; ?></h3>
-              <?php endif; ?>
-              <?php if ($row['untertitel_2']) : ?>
-                <h5><?php echo $row['untertitel_2']; ?></h5>
-              <?php endif; ?>
-              <?php if ($row['text']) : ?>
-                <p><?php echo $row['text']; ?></p>
-              <?php endif; ?>
-            <?php } ?>
-          </div>
-        <?php } ?>
-      </div>
-    </div>
+	
+	  <div class="grayplate-text">
+		<?php 
+		if ( $rows ) {
+			?>
+		  <div class="grayplate-text__wrap">
+		  <h2><?php echo $title; ?></h2>
+			<?php foreach ( $rows as $row ) { ?>
+				<?php if ( $row['untertitel_1'] ) : ?>
+				<h3><?php echo $row['untertitel_1']; ?></h3>
+			  <?php endif; ?>
+				<?php if ( $row['untertitel_2'] ) : ?>
+				<h5><?php echo $row['untertitel_2']; ?></h5>
+			  <?php endif; ?>
+				<?php if ( $row['text'] ) : ?>
+				<p><?php echo $row['text']; ?></p>
+			  <?php endif; ?>
+			<?php } ?>
+		  </div>
+		<?php } ?>
+	  </div>
+	</div>
   </section>
-<?php
+	<?php
 endif;
