a0250a2bb750540c0fdf32488fdaf5cf9344dfeb
vovaent <vovaent@gmail.com>
2023-07-17 15:24:41 +0300

    Fix mu-switcher

--- js/components/measure-units-switcher/measure-units-switcher.js
+++ js/components/measure-units-switcher/measure-units-switcher.js
@@ -9,15 +9,10 @@ const muSwitcher = ( $ ) => {
 
 	$muFlag.on( 'click', function () {
 		const $thisFlag = $( this );
-
 		const thisLangCode = $thisFlag.data( 'lang-code' );
+		const muFlagItemId = $thisFlag.data( 'item-id' );
 
-		const muFlagItemId =
-			$thisFlag.data( 'item-id' ) !== null
-				? $thisFlag.data( 'item-id' )
-				: null;
-
-		if ( muFlagItemId !== null ) {
+		if ( typeof muFlagItemId !== 'undefined' ) {
 			$( `.mu-value[data-item-id="${ muFlagItemId }"]` ).hide();
 			$(
 				`.mu-value[data-lang-code="${ thisLangCode }"][data-item-id="${ muFlagItemId }"]`
