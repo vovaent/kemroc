4827dfa9911947922f54ace35e8e361e1a1ccd9d
vovaent <vovaent@gmail.com>
2023-07-19 00:05:13 +0300

    Added multi-level nesting of menus

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -116,14 +116,10 @@ add_filter(
 				// append icon.
 				if ( $image && $item_thumb_id ) {
 			
-					$item->title = $item_thumb . '<div class="item-title"><span>' . $item->title . '</span><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L5.70711 4.29289C6.09763 4.68342 6.09763 5.31658 5.70711 5.70711L1.70711 9.70711C1.31658 10.0976 0.683417 10.0976 0.292893 9.70711C-0.0976311 9.31658 -0.0976311 8.68342 0.292893 8.29289L3.58579 5L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z" fill="#444444"/>
-                    </svg></div>';
+					$item->title = $item_thumb . '<div class="item-title arrow-right"><span>' . $item->title . '</span></div>';
 			
 				} else {
-					$item->title = '<span>' . $item->title . '</span><svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L5.70711 4.29289C6.09763 4.68342 6.09763 5.31658 5.70711 5.70711L1.70711 9.70711C1.31658 10.0976 0.683417 10.0976 0.292893 9.70711C-0.0976311 9.31658 -0.0976311 8.68342 0.292893 8.29289L3.58579 5L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z" fill="#444444"/>
-                </svg>';
+					$item->title = '<div class="item-title arrow-right"><span>' . $item->title . '</span></div>';
 				}
 			}       
 		}
--- js/components/navigation/navigation.js
+++ js/components/navigation/navigation.js
@@ -1,15 +1,19 @@
 const navigation = ( $ ) => {
 	$( '.header__mobile .main-navigation li' ).each( function () {
 		const backTitle = $( this ).find( '>:first-child' ).text();
+		// const backTitle = 'back';
+
+		$( $( this ).find( '>.sub-menu > li' ) ).wrapAll(
+			'<div class="sub-menu__items-wrapper"></div>'
+		);
+
 		$(
 			'<li class="nav-back"><span class="back-btn">' +
 				backTitle +
 				'</span><button class="toggle-nav"></button></li>'
-		).insertBefore( $( this ).find( '.sub-menu > li:first-child' ) );
-		$( $( this ).find( '.sub-menu > li:not(:first-child)' ) ).wrapAll(
-			'<div class="sub-menu__items-wrapper"></div>'
-		);
+		).insertBefore( $( this ).find( '.sub-menu__items-wrapper' ) );
 	} );
+
 	$( document ).on( 'click', '.toggle-nav', function ( e ) {
 		e.preventDefault();
 		if ( $( '.header__mobile' ).hasClass( 'nav-active' ) ) {
@@ -18,14 +22,16 @@ const navigation = ( $ ) => {
 			$( '.header__mobile' ).addClass( 'nav-active' );
 		}
 	} );
+
 	$( document ).on(
 		'click',
 		'.header__mobile .nav-back .back-btn',
 		function ( e ) {
 			e.preventDefault();
-			$( this ).parents( '.sub-menu' ).removeClass( 'active' );
+			$( $( this ).parents( '.sub-menu' )[ 0 ] ).removeClass( 'active' );
 		}
 	);
+
 	$( document ).on(
 		'click',
 		'.header__mobile li.menu-item-has-children > a',
@@ -43,7 +49,8 @@ const navigation = ( $ ) => {
 
 	$liHasChildren.each( function () {
 		const $this = $( this );
-		const $thisSubMenu = $( '.sub-menu', $this );
+		const $thisSubMenu = $( '>.sub-menu', $this );
+		console.log( $thisSubMenu );
 		const $thisSubMenuWrapper = $(
 			'.sub-menu__items-wrapper li:first',
 			$thisSubMenu
